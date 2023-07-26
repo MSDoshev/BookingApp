067e84562e2daa307fbf2f72c8283769cbdf1354
MSDoshev <mihaildo@outlook.com>
2023-07-26 13:17:58 +0300

    Project setup

---
+++ client/booking-app/src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import styles from "./Header.module.css"
+import { useState } from "react"
+
+
+
+export const Header = () => {
+const [click, setClick] = useState(false);
+
+const handleClick = () => setClick(!click);
+
+
+return(
+    <nav>
+        <div className={styles.navbar}>
+            
+            <ul className={styles.links}>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Gallery</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Contact</a></li>
+            </ul>
+            <div className={styles.logo}><a href="/">Mykonos Spa & Resort</a></div>
+            <a href='/' className={styles.actionBtn}>Book Now</a>
+            <div className={styles.toggleBtn} onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}/>
+            </div>
+        </div>
+
+        <div className={`${styles.dropdownMenu} ${click ? styles.open : ""}`}>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Gallery</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Contact</a></li>
+            <li><a href='/' className={styles.actionBtn}>Book Now</a></li>
+        </div>
+    </nav> 
+);
+
+}
