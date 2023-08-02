85984eb73a18face89bd1da7a88d928f94270246
MSDoshev <mihaildo@outlook.com>
2023-08-02 16:24:09 +0300

    add(aboutUs)
    add about us section on home page

--- client/booking-app/src/components/Home/Home.js
+++ client/booking-app/src/components/Home/Home.js
@@ -5,7 +5,7 @@ export const Home = () => {
 
 
     return (
-        
+        <>
         <section className={styles.slogan}>
             <h1>Abort! Quit the job and come stay at our Mykonos Spa and Resort.</h1>
             <div className={styles.btnGallery}>
@@ -13,6 +13,48 @@ export const Home = () => {
             </div>
 
         </section>
+
+        <section className={styles.about}>
+        <h2>About Us</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam venenatis quis ex id mollis. 
+            Quisque volutpat efficitur sagittis. Suspendisse quis scelerisque eros. Nam vel leo magna. 
+            Praesent hendrerit justo sapien, ac varius felis pharetra ultricies. 
+            Morbi eleifend eros lacus, sit amet efficitur erat aliquam quis. Donec arcu lectus, 
+            dapibus sed tortor in, euismod gravida metus. In molestie in metus eu cursus. 
+            Curabitur at molestie ante, in vulputate lacus. Phasellus turpis urna, sollicitudin non eleifend non, 
+            sagittis id libero. Quisque mollis velit id gravida aliquam. Curabitur imperdiet dapibus ex molestie egestas. 
+            Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis quis fermentum nisl. 
+            Quisque feugiat nisi at augue vulputate viverra.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam venenatis quis ex id mollis. 
+            Quisque volutpat efficitur sagittis. Suspendisse quis scelerisque eros. Nam vel leo magna. 
+            Praesent hendrerit justo sapien, ac varius felis pharetra ultricies. 
+            Morbi eleifend eros lacus, sit amet efficitur erat aliquam quis. Donec arcu lectus, 
+            dapibus sed tortor in, euismod gravida metus. In molestie in metus eu cursus. 
+            Curabitur at molestie ante, in vulputate lacus. Phasellus turpis urna, sollicitudin non eleifend non, 
+            sagittis id libero. Quisque mollis velit id gravida aliquam. Curabitur imperdiet dapibus ex molestie egestas. 
+            Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis quis fermentum nisl. 
+            Quisque feugiat nisi at augue vulputate viverra.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam venenatis quis ex id mollis. 
+            Quisque volutpat efficitur sagittis. Suspendisse quis scelerisque eros. Nam vel leo magna. 
+            Praesent hendrerit justo sapien, ac varius felis pharetra ultricies. 
+            Morbi eleifend eros lacus, sit amet efficitur erat aliquam quis. Donec arcu lectus, 
+            dapibus sed tortor in, euismod gravida metus. In molestie in metus eu cursus. 
+            Curabitur at molestie ante, in vulputate lacus. Phasellus turpis urna, sollicitudin non eleifend non, 
+            sagittis id libero. Quisque mollis velit id gravida aliquam. Curabitur imperdiet dapibus ex molestie egestas. 
+            Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis quis fermentum nisl. 
+            Quisque feugiat nisi at augue vulputate viverra.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam venenatis quis ex id mollis. 
+            Quisque volutpat efficitur sagittis. Suspendisse quis scelerisque eros. Nam vel leo magna. 
+            Praesent hendrerit justo sapien, ac varius felis pharetra ultricies. 
+            Morbi eleifend eros lacus, sit amet efficitur erat aliquam quis. Donec arcu lectus, 
+            dapibus sed tortor in, euismod gravida metus. In molestie in metus eu cursus. 
+            Curabitur at molestie ante, in vulputate lacus. Phasellus turpis urna, sollicitudin non eleifend non, 
+            sagittis id libero. Quisque mollis velit id gravida aliquam. Curabitur imperdiet dapibus ex molestie egestas. 
+            Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis quis fermentum nisl. 
+            Quisque feugiat nisi at augue vulputate viverra.</p>
+        </section>
+
+        </>
         
     );
 };
