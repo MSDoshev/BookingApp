c8f00abc235a9cc6f05ff6b18ce71f0c8c495d13
MSDoshev <mihaildo@outlook.com>
2023-08-03 18:03:21 +0300

    update(galleryPage)
    make the gallery functional, we can now open/close the images, go to next/prev image

--- client/booking-app/src/components/Gallery/Gallery.js
+++ client/booking-app/src/components/Gallery/Gallery.js
@@ -1,6 +1,7 @@
 import styles from "./Gallery.module.css"
 // import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css'
+import { useState } from "react";
 
 export const Gallery = () => {
     const images = [
@@ -9,11 +10,11 @@ export const Gallery = () => {
         alt: 'Image 1 description',
       },
       {
-        url: 'https://s3-eu-central-1.amazonaws.com/loggia-cdn/lodgeContent/164b11225e28a38808a5b62d6808f439.webp',
+        url: 'https://www.vacation-key.com/photos/1/0/106720-1.jpg',
         alt: 'Image 2 description',
       },
       {
-        url: 'https://s3-eu-central-1.amazonaws.com/loggia-cdn/lodgeContent/164b11225e28a38808a5b62d6808f439.webp',
+        url: 'https://media-cdn.tripadvisor.com/media/photo-s/1b/d3/ba/26/m-one-villa-private-pool.jpg',
         alt: 'Image 3 description',
       },
       {
@@ -33,17 +34,54 @@ export const Gallery = () => {
         alt: 'Image 7 description',
       },
     ];
+
+
+    const [showModal, setShowModal] = useState(false);
+    const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+    const openModal = (index) => {
+        setShowModal(true);
+        setSelectedImageIndex(index);
+      };
+    
+      const closeModal = () => {
+        setShowModal(false);
+      };
+    
+      const navigateImage = (direction) => {
+        if (direction === 'prev') {
+          setSelectedImageIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : images.length - 1));
+        } else if (direction === 'next') {
+          setSelectedImageIndex((prevIndex) => (prevIndex < images.length - 1 ? prevIndex + 1 : 0));
+        }
+      };
+  
+
   
     return (
         <>
         <div className={styles.title}>
             <h2> The Gallery</h2>
         </div>
-      <div className={styles.gallery}>
+        <div className={styles.gallery}>
         {images.map((image, index) => (
-          <img key={index} src={image.url} alt={image.alt} />
+          <img key={index} src={image.url} alt={image.alt} onClick={() => openModal(index)} />
         ))}
       </div>
+      {showModal && (
+        <div className={styles.modal}>
+          <span className={styles.close} onClick={closeModal}>
+            &times;
+          </span>
+          <img className={styles.modalContent} src={images[selectedImageIndex].url} alt={images[selectedImageIndex].alt} />
+          <span className={styles.prev} onClick={() => navigateImage('prev')}>
+            &#10094;
+          </span>
+          <span className={styles.next} onClick={() => navigateImage('next')}>
+            &#10095;
+          </span>
+        </div>
+        )}
       </>
     );
   };
