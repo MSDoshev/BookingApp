4cd9b5c4e779919fce0979a663f2f678a18f4cf9
MSDoshev <mihaildo@outlook.com>
2023-10-11 17:07:29 +0300

    refactor(Villas Page)
    Create separate Villa component and map to display multiple villas

---
+++ client/booking-app/src/components/Villas/Villa/Villa.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import styles from '../Villas.module.css'; 
+
+const Villa = ({ villaData }) => {
+  return (
+    <div className={styles.villa}>
+      <div className={styles.villaContent}>
+        <h2>{villaData.title}</h2>
+        <p>{villaData.description}</p>
+        <div className={styles.vInfo}>
+          <h3>Up to {villaData.capacity} people</h3>
+          <h3>Price from &euro;{villaData.price}</h3>
+          <Link to={`/villa/${villaData.id}`}>Book Now</Link>
+        </div>
+      </div>
+      <img src={villaData.image} alt={villaData.title} />
+    </div>
+  );
+};
+
+export default Villa;
--- client/booking-app/src/components/Villas/Villas.js
+++ client/booking-app/src/components/Villas/Villas.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import styles from './Villas.module.css'
 import { useEffect, useState } from 'react';
+import Villa from './Villa/Villa';
 
 export const Villas = () => {
 
@@ -16,74 +17,70 @@ export const Villas = () => {
       return Math.floor(Math.random() * 11) * 10;
     }
 
+    const villasData = [
+        {
+          id: 1,
+          title: 'Villa №1',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet ligula a sem pulvinar molestie. Aliquam erat volutpat. Curabitur faucibus accumsan odio sed semper. Fusce molestie eget nisi eu elementum. Nam consectetur ipsum eu mollis aliquam. Sed accumsan sollicitudin eros a euismod. Maecenas pretium velit in nisi condimentum pharetra. Nullam ac est nisi. Nunc in ligula quam. Proin in aliquam lectus.',
+          capacity: 6,
+          price: 1000,
+          image: 'https://cdn.lecollectionist.com/lc/production/uploads/photos/house-4309/2021-04-16-eaeb829ab2eebc2b7f26bff0d1505f86.jpg?q=65',
+        },
+        {
+          id: 2,
+          title: 'Villa №2',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet ligula a sem pulvinar molestie. Aliquam erat volutpat. Curabitur faucibus accumsan odio sed semper. Fusce molestie eget nisi eu elementum. Nam consectetur ipsum eu mollis aliquam. Sed accumsan sollicitudin eros a euismod. Maecenas pretium velit in nisi condimentum pharetra. Nullam ac est nisi. Nunc in ligula quam. Proin in aliquam lectus.',
+          capacity: 4,
+          price: 800,
+          image: 'https://cdn.lecollectionist.com/lc/production/uploads/photos/house-4309/2021-04-16-eaeb829ab2eebc2b7f26bff0d1505f86.jpg?q=65',
+        },
+        {
+          id: 3,
+          title: 'Villa №3',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet ligula a sem pulvinar molestie. Aliquam erat volutpat. Curabitur faucibus accumsan odio sed semper. Fusce molestie eget nisi eu elementum. Nam consectetur ipsum eu mollis aliquam. Sed accumsan sollicitudin eros a euismod. Maecenas pretium velit in nisi condimentum pharetra. Nullam ac est nisi. Nunc in ligula quam. Proin in aliquam lectus.',
+          capacity: 4,
+          price: 800,
+          image: 'https://cdn.lecollectionist.com/lc/production/uploads/photos/house-4309/2021-04-16-eaeb829ab2eebc2b7f26bff0d1505f86.jpg?q=65',
+        },
+        {
+          id: 4,
+          title: 'Villa №4',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet ligula a sem pulvinar molestie. Aliquam erat volutpat. Curabitur faucibus accumsan odio sed semper. Fusce molestie eget nisi eu elementum. Nam consectetur ipsum eu mollis aliquam. Sed accumsan sollicitudin eros a euismod. Maecenas pretium velit in nisi condimentum pharetra. Nullam ac est nisi. Nunc in ligula quam. Proin in aliquam lectus.',
+          capacity: 2,
+          price: 500,
+          image: 'https://cdn.lecollectionist.com/lc/production/uploads/photos/house-4309/2021-04-16-eaeb829ab2eebc2b7f26bff0d1505f86.jpg?q=65',
+        },
+        {
+          id: 5,
+          title: 'Villa №5',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet ligula a sem pulvinar molestie. Aliquam erat volutpat. Curabitur faucibus accumsan odio sed semper. Fusce molestie eget nisi eu elementum. Nam consectetur ipsum eu mollis aliquam. Sed accumsan sollicitudin eros a euismod. Maecenas pretium velit in nisi condimentum pharetra. Nullam ac est nisi. Nunc in ligula quam. Proin in aliquam lectus.',
+          capacity: 2,
+          price: 500,
+          image: 'https://cdn.lecollectionist.com/lc/production/uploads/photos/house-4309/2021-04-16-eaeb829ab2eebc2b7f26bff0d1505f86.jpg?q=65',
+        },
+        {
+          id: 6,
+          title: 'Villa №6',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet ligula a sem pulvinar molestie. Aliquam erat volutpat. Curabitur faucibus accumsan odio sed semper. Fusce molestie eget nisi eu elementum. Nam consectetur ipsum eu mollis aliquam. Sed accumsan sollicitudin eros a euismod. Maecenas pretium velit in nisi condimentum pharetra. Nullam ac est nisi. Nunc in ligula quam. Proin in aliquam lectus.',
+          capacity: 2,
+          price: 500,
+          image: 'https://cdn.lecollectionist.com/lc/production/uploads/photos/house-4309/2021-04-16-eaeb829ab2eebc2b7f26bff0d1505f86.jpg?q=65',
+        },
+        
+      ];
+
     return(
         <>
-            <div className={styles.title}>
-                <h1>Villas</h1>
-            </div>
-            <div className={styles.villas}>
-                
-                <div className={styles.villa}>
-                    <div className={styles.villaContent}> 
-                        <h2>Villa №1</h2>
-                        
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet ligula a sem pulvinar molestie. Aliquam erat volutpat. Curabitur faucibus accumsan odio sed semper. Fusce molestie eget nisi eu elementum. Nam consectetur ipsum eu mollis aliquam. Sed accumsan sollicitudin eros a euismod. Maecenas pretium velit in nisi condimentum pharetra. Nullam ac est nisi. Nunc in ligula quam. Proin in aliquam lectus.</p>
-                        <div className={styles.vInfo}>
-                                <h3>Up to 6 people</h3>
-                                <h3>Price from &euro;1000</h3>
-                                <Link to="room-id" >Book Now</Link>
-                            </div>
-                    
-                    </div>
-                    
-                        
-                    <img src='https://cdn.lecollectionist.com/lc/production/uploads/photos/house-4309/2021-04-16-eaeb829ab2eebc2b7f26bff0d1505f86.jpg?q=65'/>
-                </div>
-                <div className={styles.villa2}>
-                    <img src='https://cdn.lecollectionist.com/lc/production/uploads/photos/house-4309/2021-04-16-eaeb829ab2eebc2b7f26bff0d1505f86.jpg?q=65'/>
-                    <div className={styles.villaContent}>
-                        <h2>Villa №1</h2>
-
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet ligula a sem pulvinar molestie. Aliquam erat volutpat. Curabitur faucibus accumsan odio sed semper. Fusce molestie eget nisi eu elementum. Nam consectetur ipsum eu mollis aliquam. Sed accumsan sollicitudin eros a euismod. Maecenas pretium velit in nisi condimentum pharetra. Nullam ac est nisi. Nunc in ligula quam. Proin in aliquam lectus.</p>
-
-                        <Link to="room-id" >Book Now</Link>
-                    
-                    </div>
-                    
-                        
-                </div>
-
-                <div className={styles.villa}>
-                    <div className={styles.villaContent}>
-                        <h2>Villa №1</h2>
-
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet ligula a sem pulvinar molestie. Aliquam erat volutpat. Curabitur faucibus accumsan odio sed semper. Fusce molestie eget nisi eu elementum. Nam consectetur ipsum eu mollis aliquam. Sed accumsan sollicitudin eros a euismod. Maecenas pretium velit in nisi condimentum pharetra. Nullam ac est nisi. Nunc in ligula quam. Proin in aliquam lectus.</p>
-
-                        <Link to="room-id" >Book Now</Link>
-                    
-                    </div>
-                    
-                        
-                    <img src='https://cdn.lecollectionist.com/lc/production/uploads/photos/house-4309/2021-04-16-eaeb829ab2eebc2b7f26bff0d1505f86.jpg?q=65'/>
-                </div>
-
-                <div className={styles.villa2}>
-                    <img src='https://cdn.lecollectionist.com/lc/production/uploads/photos/house-4309/2021-04-16-eaeb829ab2eebc2b7f26bff0d1505f86.jpg?q=65'/>
-                    <div className={styles.villaContent}>
-                        <h2>Villa №1</h2>
-
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet ligula a sem pulvinar molestie. Aliquam erat volutpat. Curabitur faucibus accumsan odio sed semper. Fusce molestie eget nisi eu elementum. Nam consectetur ipsum eu mollis aliquam. Sed accumsan sollicitudin eros a euismod. Maecenas pretium velit in nisi condimentum pharetra. Nullam ac est nisi. Nunc in ligula quam. Proin in aliquam lectus.</p>
-
-                        <Link to="room-id" >Book Now</Link>
-                    
-                    </div>
-                    
-                        
-                </div>
-
-
-            </div>
-        </>
+        <div className={styles.title}>
+          <h1>Villas</h1>
+        </div>
+        <div className={styles.villas}>
+          {villasData.map((villa) => (
+            <Villa key={villa.id} villaData={villa} />
+          ))}
+        </div>
+      </>
     )
 
 }
+
