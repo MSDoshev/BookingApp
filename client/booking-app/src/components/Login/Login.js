7e993b839a3cc8d30b30120646062cfbffb791ef
MSDoshev <mihaildo@outlook.com>
2024-04-19 18:55:25 +0300

    Handle login

--- client/booking-app/src/components/Login/Login.js
+++ client/booking-app/src/components/Login/Login.js
@@ -1,22 +1,66 @@
 import PageTransitionAnimation from "../PageTransitionAnimation/PageTransitionAnimation";
 import styles from "./Login.module.css";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { loginUser } from "../../store/login-actions";
 
 export default function Login() {
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { isAuthenticated } = useSelector((state) => state.auth); //error
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(loginUser(userData.email, userData.password));
+  };
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/");
+    }
+  }, [isAuthenticated, navigate]);
+
   return (
     <PageTransitionAnimation>
       <h1 className={styles.title}>Login</h1>
       <div className={styles.loginFormContainer}>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">Email:</label>
           <input
             type="email"
             placeholder="example@mail.com"
             name="email"
             id="email"
+            value={userData.email}
+            onChange={handleInputChange}
           ></input>
-          <label>Password:</label>
-          <input type="password" placeholder="***********"></input>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            placeholder="***********"
+            name="password"
+            id="password"
+            value={userData.password}
+            onChange={handleInputChange}
+          ></input>
+          <button type="submit">Submit</button>
         </form>
         <div>
           <p>
