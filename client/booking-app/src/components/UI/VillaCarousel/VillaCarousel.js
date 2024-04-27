33a2daec2eefa1ec533638cc69024cb6ed19222b
MSDoshev <mihaildo@outlook.com>
2024-04-27 19:27:38 +0300

    add styling
    - add stylized scrollbar
    - minor fixes

--- client/booking-app/src/components/UI/VillaCarousel/VillaCarousel.js
+++ client/booking-app/src/components/UI/VillaCarousel/VillaCarousel.js
@@ -23,14 +23,15 @@ const VillaCarousel = ({ images }) => {
         key={currentImageIndex}
         initial={{
           opacity: 0,
+          
         }}
         animate={{
           opacity: 1,
-          transition: { duration: 0.5 },
+          transition: { duration: 1 },
         }}
         exit={{
           opacity: 0,
-          transition: { duration: 0.5 },
+          transition: { duration: 1 },
         }}
         src={images[currentImageIndex]}
         alt="Villa"
