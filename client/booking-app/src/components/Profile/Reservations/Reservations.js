5cb3705f05bab653a3eba507a0280ccbca956116
MSDoshev <mihaildo@outlook.com>
2024-04-30 13:30:01 +0300

    update reservations
    - conditionally display user reservations or placeholder

--- client/booking-app/src/components/Profile/Reservations/Reservations.js
+++ client/booking-app/src/components/Profile/Reservations/Reservations.js
@@ -18,44 +18,51 @@ export default function Reservations({ reservations, setReservations }) {
     }));
   }
   return (
-    <motion.ul
-      animate={{ opacity: [0, 1], y: [100, 0] }}
-      transition={{ duration: 0.5 }}
-      className={styles.reservationsContainerAll}
-    >
-      {reservations &&
-        Object.entries(reservations).map(([key, reservation]) => (
-          <li key={key} className={styles.reservationContainer}>
-            <img src={reservation.villa.image.url} alt="" />
-            <div className={styles.reservationTextInfo}>
-              <h2>{reservation.villa.title}</h2>
-              <div className={styles.reservationInfo}>
-                <p>
-                  <span>Guests: </span>
-                  {reservation.guests}
-                </p>
-                <p>
-                  <span>From: </span>
-                  {reservation.dateFrom}
-                </p>
-                <p>
-                  <span>To: </span>
-                  {reservation.dateTo}
-                </p>
-                <p>
-                  <span>Price: </span>
-                  {reservation.villa.price}
-                </p>
+    <>
+      {reservations && Object.values(reservations).length > 0 ? (
+        <motion.ul
+          animate={{ opacity: [0, 1], y: [100, 0] }}
+          transition={{ duration: 0.5 }}
+          className={styles.reservationsContainerAll}
+        >
+          {Object.entries(reservations).map(([key, reservation]) => (
+            <li key={key} className={styles.reservationContainer}>
+              <img src={reservation.villa.image.url} alt="" />
+              <div className={styles.reservationTextInfo}>
+                <h2>{reservation.villa.title}</h2>
+                <div className={styles.reservationInfo}>
+                  <p>
+                    <span>Guests: </span>
+                    {reservation.guests}
+                  </p>
+                  <p>
+                    <span>From: </span>
+                    {reservation.dateFrom}
+                  </p>
+                  <p>
+                    <span>To: </span>
+                    {reservation.dateTo}
+                  </p>
+                  <p>
+                    <span>Price: </span>
+                    {reservation.villa.price}
+                  </p>
+                </div>
+                <motion.button
+                  whileHover={{ scale: 1.1 }}
+                  onClick={() => handleCancel(key)}
+                >
+                  Cancel
+                </motion.button>
               </div>
-              <motion.button
-                whileHover={{ scale: 1.1 }}
-                onClick={() => handleCancel(key)}
-              >
-                Cancel
-              </motion.button>
-            </div>
-          </li>
-        ))}
-    </motion.ul>
+            </li>
+          ))}
+        </motion.ul>
+      ) : (
+        <div className={styles.placeholder}>
+          <p>No reservations yet..</p>
+        </div>
+      )}
+    </>
   );
 }
