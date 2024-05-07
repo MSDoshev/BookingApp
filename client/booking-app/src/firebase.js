f5d13cfe0ece7dfc5908cf157faae0b6509c1ca1
MSDoshev <mihaildo@outlook.com>
2024-05-07 15:43:25 +0300

    bug fix

--- client/booking-app/src/firebase.js
+++ client/booking-app/src/firebase.js
@@ -1,25 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  browserLocalPersistence,
-  setPersistence,
-} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, browserLocalPersistence, setPersistence } from "firebase/auth";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCoBz8zpOMvEOlVckam1xDQX4dbkMllPFs",
-  authDomain: "bookingapp-effea.firebaseapp.com",
-  databaseURL:
-    "https://bookingapp-effea-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "bookingapp-effea",
-  storageBucket: "bookingapp-effea.appspot.com",
-  messagingSenderId: "629485352111",
-  appId: "1:629485352111:web:ad29e990a28463f1ab105a",
-  measurementId: "G-2YK7DBRM4K",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -30,4 +21,3 @@ export const auth = getAuth(app);
 setPersistence(auth, browserLocalPersistence);
 
 auth.onAuthStateChanged(() => {});
-
